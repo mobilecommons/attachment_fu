ad3b28542634cbaad7fd7720aa01e1baac5eca50
Travis Graham <28802+tgraham@users.noreply.github.com>
2024-10-03 10:48:05 -0400

    Psych safe load aliases override

--- lib/technoweenie/attachment_fu/backends/s3_backend.rb
+++ lib/technoweenie/attachment_fu/backends/s3_backend.rb
@@ -184,7 +184,7 @@ module Technoweenie # :nodoc:
 
           begin
             @@s3_config_path = base.attachment_options[:s3_config_path] || File.join(Rails.root, 'config', 'amazon_s3.yml')
-            @@s3_config = @@s3_config = YAML.load(ERB.new(File.read(@@s3_config_path)).result)[Rails.env].symbolize_keys
+            @@s3_config = @@s3_config = YAML.load(ERB.new(File.read(@@s3_config_path)).result, aliases: true)[Rails.env].symbolize_keys
           #rescue
           #  raise ConfigFileNotFoundError.new('File %s not found' % @@s3_config_path)
           end
